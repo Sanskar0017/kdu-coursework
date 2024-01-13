311aac674d02685610bdd15d25c21ac4e5973e89
Sanskar0017 <sanskarkumar.saraf@kickdrumtech.com>
2024-01-13 11:05:57 +0530

    backend homework updated

--- backend/homework/5/src/main/java/org/backend1/Main.java
+++
@@ -1,7 +0,0 @@
-package org.backend1;
-
-public class Main {
-    public static void main(String[] args) {
-        throw new UnsupportedOperationException("main method is not implemented.");
-    }
-}
