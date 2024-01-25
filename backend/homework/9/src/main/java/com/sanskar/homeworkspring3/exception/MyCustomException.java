5aff51d2df5aa6dfe0e416272c4e23d087dde018
Sanskar0017 <sanskarkumar.saraf@kickdrumtech.com>
2024-01-25 21:37:31 +0530

    refactoring branch

--- backend/homework/9/src/main/java/com/sanskar/homeworkspring3/exception/MyCustomException.java
+++
@@ -1,7 +0,0 @@
-package com.sanskar.homeworkspring3.exception;
-
-public class MyCustomException extends IndexOutOfBoundsException{
-    public MyCustomException(String s) {
-        super(s);
-    }
-}
