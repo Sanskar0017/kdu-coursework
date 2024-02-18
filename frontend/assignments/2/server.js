4d40e76c3702d3bbdf4eec103f30064870c571d3
Sanskar0017 <sanskarkumar.saraf@kickdrumtech.com>
2024-02-18 22:06:47 +0530

    login completed and active-user extraction completed

---
+++ frontend/assignments/2/server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const socketIo = require("socket.io");
+const { loginUser } = require("./Controller/UserRoutes");
+const User = require("./models/user");
+const { log } = require("console");
+
+const app = express();
+const server = http.createServer(app);
+const io = new socketIo.Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+
+const activeUsers = new Set();
+const userObj = new User();
+
+app.post("/api/user/login", (req, res) => {
+  const { username, password } = req.body;
+  console.log("api/user/login");
+
+  const user = userObj.verifyUser(username, password);
+  console.log("server.js");
+  console.log(user);
+  if (user.user_name == username) {
+    console.log("fetching vaidation");
+    activeUsers.add(user.id);
+    res.json({ message: "Login successful", user });
+    console.log("response successful");
+  } else {
+    res.status(401).json({ error: "Invalid username or password" });
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("userConnected", (username) => {
+    console.log("Adding user to active users", username);
+    activeUsers.add(username);
+    io.emit("activeUsers", Array.from(activeUsers));
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    activeUsers.delete(socket.username);
+    io.emit(
+      "activeUsers",
+      Array.from(activeUsers).map((userId) => {
+        return userObj.getUserById(userId).user_name;
+      })
+    );
+  });
+
+  socket.on("chatMessage", (message) => {
+    console.log("Input message on server side is:", message);
+    io.emit("chatMessage", message);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
